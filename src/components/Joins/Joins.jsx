8c7c2b3c8860e6467f3357fbc1b3e42cf377a6e7
wubaiqing <wubaiqing@vip.qq.com>
2016-08-07 20:16:26 +0800

    build success

--- src/components/Joins/Joins.jsx
+++ src/components/Joins/Joins.jsx
@@ -8,12 +8,18 @@ import commons from '../Common/Common.less';
 import styles from './Joins.less';
 
 // 表单
-import { Form, Input, Button, Checkbox, Steps, Row, Col } from 'antd';
+import { Form, Input, Button, Checkbox, Steps, Row, Col, Spin, message } from 'antd';
 const FormItem = Form.Item;
 const CreateForm = Form.create;
 
 
 let Joins = React.createClass({
+  getInitialState() {
+    return { loading: false };
+  },
+  toggle(value) {
+    this.setState({ loading: value });
+  },
   // 重置
   handleReset(e) {
     e.preventDefault();
@@ -23,16 +29,22 @@ let Joins = React.createClass({
   // 提交
   handleSubmit(e) {
     e.preventDefault();
-    console.log('收到表单值：', this.props.form.getFieldsValue());
+    let input = this;
     this.props.form.validateFields((errors, values) => {
       if (!!errors) {
         return;
       }
-    });
-    $.getJSON('http://www.taokedaniu.com', (data) => {
-      console.dir(data);
-
-
+      input.state.loading = true;
+      $.getJSON('http://article.app/api/supplier', {...this.props.form.getFieldsValue()}, (data) => {
+        if (data.status) {
+          input.state.loading = false;
+          message.success('添加成功');
+          input.props.form.resetFields();
+        } else {
+          message.error('创建失败，请联系QQ反馈');
+          input.state.loading = false;
+        }
+      });
     });
   },
 
@@ -74,7 +86,7 @@ let Joins = React.createClass({
     });
 
 
-    const telePhone = getFieldProps('tele_phone', {
+    const telePhone = getFieldProps('telephone', {
       initialValue: '',
       rules: [
         { required: true, message: '请填写联系电话' },
@@ -90,35 +102,37 @@ let Joins = React.createClass({
 
     return (
 
-        <Row className={commons.explain}>
-          <Col>
-          <Form horizontal form={this.props.form}>
-            <FormItem label="宝贝链接" {...formItemLayout}>
-              <Input {...taobaoUrlProps} placeholder="" />
-            </FormItem>
-            <FormItem label="最高佣金比例" {...formItemLayout}>
-              <Input {...maxCommissionRate}  placeholder="不是最高佣金比例，不予推广" />
-            </FormItem>
-            <FormItem label="高佣金计划申请地址" {...formItemLayout}>
-              <Input {...maxCommissionUrl} placeholder="" />
-            </FormItem>
-            <FormItem label="佣金计划审核联系人QQ" {...formItemLayout}>
-              <Input {...QQ} placeholder="三项联系信息必须全填，避免长时间不审核计划，还联系不到人" />
-            </FormItem>
-            <FormItem label="联系电话" {...formItemLayout}>
-              <Input {...telePhone} placeholder="" />
-            </FormItem>
-            <FormItem label="联系微信" {...formItemLayout}>
-              <Input {...wechat} placeholder="" />
-            </FormItem>
-            <FormItem wrapperCol={{ span: 16, offset: 6 }} style={{ marginTop: 24 }}>
-              <Button type="primary" onClick={this.handleSubmit}>提交</Button>
-              &nbsp;&nbsp;&nbsp;
-              <Button type="ghost" onClick={this.handleReset}>重置</Button>
-            </FormItem>
-          </Form>
-          </Col>
-        </Row>
+        <Spin spinning={this.state.loading}>
+          <Row className={commons.explain}>
+            <Col>
+            <Form horizontal form={this.props.form}>
+              <FormItem label="宝贝链接" {...formItemLayout}>
+                <Input {...taobaoUrlProps} placeholder="" />
+              </FormItem>
+              <FormItem label="最高佣金比例" {...formItemLayout}>
+                <Input {...maxCommissionRate}  placeholder="不是最高佣金比例，不予推广" />
+              </FormItem>
+              <FormItem label="高佣金计划申请地址" {...formItemLayout}>
+                <Input {...maxCommissionUrl} placeholder="" />
+              </FormItem>
+              <FormItem label="佣金计划审核联系人QQ" {...formItemLayout}>
+                <Input {...QQ} placeholder="三项联系信息必须全填，避免长时间不审核计划，还联系不到人" />
+              </FormItem>
+              <FormItem label="联系电话" {...formItemLayout}>
+                <Input {...telePhone} placeholder="" />
+              </FormItem>
+              <FormItem label="联系微信" {...formItemLayout}>
+                <Input {...wechat} placeholder="" />
+              </FormItem>
+              <FormItem wrapperCol={{ span: 16, offset: 6 }} style={{ marginTop: 24 }}>
+                <Button type="primary" onClick={this.handleSubmit}>提交</Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button type="ghost" onClick={this.handleReset}>重置</Button>
+              </FormItem>
+            </Form>
+            </Col>
+          </Row>
+        </Spin>
       );
   }
 });
